8bc750206c2dacd8d45de1e91f84ec82ead57463
s-a-sakib <shahrearalsakib16@gmail.com>
2023-03-30 00:52:26 +0600

    method added

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,7 @@
 public class Calculator{
+    public float addition(float a , float b){
+        return a + b;
+    }
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
     }
