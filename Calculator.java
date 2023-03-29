42ad98df1df78de4ca2dd40cc715417899653f5c
s-a-sakib <shahrearalsakib16@gmail.com>
2023-03-30 00:59:55 +0600

    subtruction method added

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,7 @@
 public class Calculator{
+    public float subtruction(float a , float b){
+        return a - b;
+    }
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
     }
